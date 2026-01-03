9e7cf22f7acb0b17f973d042163370be65997cf8
adespaignet <adespaignet@gmail.com>
2026-01-03 18:33:16 +0400

    Fix SW registration timing - use registerImmediately for update detection

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { SwUpdate } from '@angular/service-worker';
 import { SupabaseService } from './supabase.service';
@@ -9,20 +9,28 @@ import { SupabaseService } from './supabase.service';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   supabase = inject(SupabaseService);
   private router = inject(Router);
   private swUpdate = inject(SwUpdate);
 
   updateAvailable = signal(false);
 
-  constructor() {
+  ngOnInit() {
     if (this.swUpdate.isEnabled) {
       this.swUpdate.versionUpdates.subscribe(event => {
+        console.log('[SW] Version event:', event.type);
         if (event.type === 'VERSION_READY') {
           this.updateAvailable.set(true);
         }
       });
+
+      // Also check for updates on init
+      this.swUpdate.checkForUpdate().then(hasUpdate => {
+        console.log('[SW] Check for update:', hasUpdate);
+      });
+    } else {
+      console.log('[SW] Service worker not enabled');
     }
   }
 
